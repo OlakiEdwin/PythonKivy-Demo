fe8a692ed6099995d47d5e01027a1d5985a97b1e
Olaki Edwin <olakiedwin@gmail.com>
2023-10-10 20:29:42 +0300

    Defined label with all the parameters

---
+++ main.py
@@ -0,0 +1,15 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import Screen
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.button import MDRectangleFlatButton
+
+class Demo(MDApp):
+
+    def build(self):
+        screen = Screen()
+
+        l = MDLabel(text="HI PEOPLE!", halign='center',
+                    theme_text_color="Custom",
+                    text_color=(0.5, 0, 0.5, 1),
+                    font_style='Caption')
